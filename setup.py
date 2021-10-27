c75ac13184f426ccf80766208b75877e8c44a4de
Ashik Zinnat Khan <ashrik@gmail.com>
2021-10-27 19:11:58 +0600

    fix: reqs

--- setup.py
+++ setup.py
@@ -7,14 +7,8 @@ here = pathlib.Path(__file__).parent.resolve()
 README = (here / 'README.md').read_text(encoding='utf-8')
 VERSION = (here / 'VERSION').read_text()
 
-parse_reqs = re.compile(r"([^# \n]*)([# \n].*)?")
-reqs = (here / 'requirements.txt').read_text(encoding='utf-8')
-requirements = [m.groups()[0]
-                for m in map(parse_reqs.match, reqs.split('\n'))
-                if m.groups()[0]]
-
 setup(
-    name='core-utils',
+    name='core_utils',
     version=VERSION,
     description='',
     long_description=README,
@@ -25,7 +19,10 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='~=3.5',
-    install_requires=requirements,
+    install_requires=[
+        "django>=2.2.17",
+        "entitlements @ git+https://github.com/MoveBigRocks/entitlements#egg=entitlements",
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
